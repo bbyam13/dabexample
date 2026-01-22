a908655cc867d31d96678966381b28f546a7a14f
bbyam13 <brendanbyam@gmail.com>
2026-01-22 15:05:44 +0000

    clear out

--- bundle/sample_example/src/pipeline/transformations/02_ai_translation.sql
+++
@@ -1,8 +0,0 @@
--- Please edit the sample below
-
-CREATE STREAMING TABLE
-    translated_game_reviews
-SELECT 
-      *, 
-      ai_translate(review, 'en') AS translated_review
-  FROM STREAM(sampled_game_reviews)
